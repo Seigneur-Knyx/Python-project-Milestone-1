f059541de59ae07824e5bfa549bc42d04d480eea
Seigneur-Knyx <114917601+Seigneur-Knyx@users.noreply.github.com>
2024-02-26 14:24:45 +0000

    Add files via upload

---
+++ Milestone 1 project/app.py
@@ -0,0 +1,63 @@
+# Incomplete app!
+
+MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+movies = []
+
+
+# You may want to create a function for this code
+def add_a_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
+
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
+
+
+# Create other functions for:
+#   - listing movies
+#   - finding movies
+def show_movies():
+    for movie in movies:
+        print_movie(movie)
+
+
+def print_movie(movie):
+    print(f"Title: {movie['title']}")
+    print(f"Director: {movie['director']}")
+    print(f"Year: {movie['year']}")
+
+
+def find_movies():
+    search_title = input("Enter the title of the movie: ")
+    for movie in movies:
+        if movie['title'] == search_title:
+            print_movie(movie)
+
+# And another function here for the user menu
+
+
+user_options = {
+    'a': add_a_movie,
+    'l': show_movies,
+    'f': find_movies
+}
+
+
+def menu():
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection in user_options:
+            selected_function = user_options[selection]
+            selected_function()
+        else:
+            print('Unknown command. Please try again.')
+
+        selection = input(MENU_PROMPT)
+
+
+menu()
+# Remember to run the user menu function at the end!
